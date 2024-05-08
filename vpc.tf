d7588116254b947ca413895b7e0156b0f61ed886
ravi2krishna <ravi2krishna@gmail.com>
2024-05-08 07:30:30 +0530

    create private subnet

--- vpc.tf
+++ vpc.tf
@@ -19,3 +19,15 @@ resource "aws_subnet" "ibm-web-sn" {
     Name = "ibm-web-subnet"
   }
 }
+
+# PRIVATE SUBNET
+resource "aws_subnet" "ibm-data-sn" {
+  vpc_id     = aws_vpc.ibm-vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+  map_public_ip_on_launch = "false"
+
+  tags = {
+    Name = "ibm-database-subnet"
+  }
+}
