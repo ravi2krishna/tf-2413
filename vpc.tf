7371332c43baa009d23acc7c2f7591e80fc9fe97
ravi2krishna <ravi2krishna@gmail.com>
2024-05-09 07:48:13 +0530

    ecomm vpc

--- vpc.tf
+++ vpc.tf
@@ -1,84 +1,84 @@
 # VPC 
-resource "aws_vpc" "ibm-vpc" {
+resource "aws_vpc" "ecomm-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "ibm"
+    Name = "ecomm"
   }
 }
 
 # PUBLIC SUBNET
-resource "aws_subnet" "ibm-web-sn" {
-  vpc_id     = aws_vpc.ibm-vpc.id
+resource "aws_subnet" "ecomm-web-sn" {
+  vpc_id     = aws_vpc.ecomm-vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-west-2a"
   map_public_ip_on_launch = "true"
 
   tags = {
-    Name = "ibm-web-subnet"
+    Name = "ecomm-web-subnet"
   }
 }
 
 # PRIVATE SUBNET
-resource "aws_subnet" "ibm-data-sn" {
-  vpc_id     = aws_vpc.ibm-vpc.id
+resource "aws_subnet" "ecomm-data-sn" {
+  vpc_id     = aws_vpc.ecomm-vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-west-2b"
   map_public_ip_on_launch = "false"
 
   tags = {
-    Name = "ibm-database-subnet"
+    Name = "ecomm-database-subnet"
   }
 }
 
 # INTERNET GATEWAY
-resource "aws_internet_gateway" "ibm-igw" {
-  vpc_id = aws_vpc.ibm-vpc.id
+resource "aws_internet_gateway" "ecomm-igw" {
+  vpc_id = aws_vpc.ecomm-vpc.id
 
   tags = {
-    Name = "ibm-internet-gateway"
+    Name = "ecomm-internet-gateway"
   }
 }
 
 # PUBLIC ROUTE TABLE
-resource "aws_route_table" "ibm-web-rt" {
-  vpc_id = aws_vpc.ibm-vpc.id
+resource "aws_route_table" "ecomm-web-rt" {
+  vpc_id = aws_vpc.ecomm-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.ibm-igw.id
+    gateway_id = aws_internet_gateway.ecomm-igw.id
   }
 
   tags = {
-    Name = "ibm-web-route-table"
+    Name = "ecomm-web-route-table"
   }
 }
 
 # PUBLIC ROUTE TABLE ASSOCIATION
-resource "aws_route_table_association" "ibm-web-rt-association" {
-  subnet_id      = aws_subnet.ibm-web-sn.id
-  route_table_id = aws_route_table.ibm-web-rt.id
+resource "aws_route_table_association" "ecomm-web-rt-association" {
+  subnet_id      = aws_subnet.ecomm-web-sn.id
+  route_table_id = aws_route_table.ecomm-web-rt.id
 }
 
 # PRIVATE ROUTE TABLE
-resource "aws_route_table" "ibm-data-rt" {
-  vpc_id = aws_vpc.ibm-vpc.id
+resource "aws_route_table" "ecomm-data-rt" {
+  vpc_id = aws_vpc.ecomm-vpc.id
 
   tags = {
-    Name = "ibm-database-route-table"
+    Name = "ecomm-database-route-table"
   }
 }
 
 # PRIVATE ROUTE TABLE ASSOCIATION
-resource "aws_route_table_association" "ibm-data-rt-association" {
-  subnet_id      = aws_subnet.ibm-data-sn.id
-  route_table_id = aws_route_table.ibm-data-rt.id
+resource "aws_route_table_association" "ecomm-data-rt-association" {
+  subnet_id      = aws_subnet.ecomm-data-sn.id
+  route_table_id = aws_route_table.ecomm-data-rt.id
 }
 
 # PUBLIC NACL
-resource "aws_network_acl" "ibm-web-nacl" {
-  vpc_id = aws_vpc.ibm-vpc.id
+resource "aws_network_acl" "ecomm-web-nacl" {
+  vpc_id = aws_vpc.ecomm-vpc.id
 
   egress {
     protocol   = "tcp"
@@ -99,19 +99,19 @@ resource "aws_network_acl" "ibm-web-nacl" {
   }
 
   tags = {
-    Name = "ibm-web-nacl"
+    Name = "ecomm-web-nacl"
   }
 }
 
 # PUBLIC NACL ASSOCIATION
-resource "aws_network_acl_association" "ibm-web-nacl-association" {
-  network_acl_id = aws_network_acl.ibm-web-nacl.id
-  subnet_id      = aws_subnet.ibm-web-sn.id
+resource "aws_network_acl_association" "ecomm-web-nacl-association" {
+  network_acl_id = aws_network_acl.ecomm-web-nacl.id
+  subnet_id      = aws_subnet.ecomm-web-sn.id
 }
 
 # PRIVATE NACL
-resource "aws_network_acl" "ibm-data-nacl" {
-  vpc_id = aws_vpc.ibm-vpc.id
+resource "aws_network_acl" "ecomm-data-nacl" {
+  vpc_id = aws_vpc.ecomm-vpc.id
 
   egress {
     protocol   = "tcp"
@@ -132,30 +132,30 @@ resource "aws_network_acl" "ibm-data-nacl" {
   }
 
   tags = {
-    Name = "ibm-data-nacl"
+    Name = "ecomm-data-nacl"
   }
 }
 
 # PRIVATE NACL ASSOCIATION
-resource "aws_network_acl_association" "ibm-data-nacl-association" {
-  network_acl_id = aws_network_acl.ibm-data-nacl.id
-  subnet_id      = aws_subnet.ibm-data-sn.id
+resource "aws_network_acl_association" "ecomm-data-nacl-association" {
+  network_acl_id = aws_network_acl.ecomm-data-nacl.id
+  subnet_id      = aws_subnet.ecomm-data-sn.id
 }
 
 # PUBLIC SECUIRTY GROUP 
-resource "aws_security_group" "ibm-web-sg" {
-    name = "ibm-web-server-sg"
+resource "aws_security_group" "ecomm-web-sg" {
+    name = "ecomm-web-server-sg"
     description = "Allows Web Server Traffic"
-    vpc_id = aws_vpc.ibm-vpc.id
+    vpc_id = aws_vpc.ecomm-vpc.id
     
     tags = {
-        Name = "ibm-web-secuirty-group"
+        Name = "ecomm-web-secuirty-group"
     }
 }
 
 # SSH TRAFFIC 
-resource "aws_vpc_security_group_ingress_rule" "ibm-web-ssh" {
-  security_group_id = aws_security_group.ibm-web-sg.id
+resource "aws_vpc_security_group_ingress_rule" "ecomm-web-ssh" {
+  security_group_id = aws_security_group.ecomm-web-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
@@ -163,8 +163,8 @@ resource "aws_vpc_security_group_ingress_rule" "ibm-web-ssh" {
 }
 
 # HTTP TRAFFIC 
-resource "aws_vpc_security_group_ingress_rule" "ibm-http-ssh" {
-  security_group_id = aws_security_group.ibm-web-sg.id
+resource "aws_vpc_security_group_ingress_rule" "ecomm-http-ssh" {
+  security_group_id = aws_security_group.ecomm-web-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
